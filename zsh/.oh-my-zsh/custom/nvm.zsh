92462ebdc2129bdb8ae1a5b4e19f54b4ca5c3b83
Vic Vijayakumar <needcaffeine@gmail.com>
2022-07-01 19:44:11 -0400

    feat: oh-my-zsh plugins and settings

---
+++ zsh/.oh-my-zsh/custom/nvm.zsh
@@ -0,0 +1,22 @@
+# NVM
+lazynvm() {
+  unset -f nvm node npm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+}
+
+nvm() {
+  lazynvm
+  nvm $@
+}
+
+node() {
+  lazynvm
+  node $@
+}
+
+npm() {
+  lazynvm
+  npm $@
+}

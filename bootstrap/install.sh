a07a112e59c107d6e76018b38e5b183c0c6f84bf
Vic Vijayakumar <needcaffeine@gmail.com>
2022-07-01 19:09:26 -0400

    feat: install rosetta 2 as part of bootstrap

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -10,8 +10,14 @@ fi
 printf '✅ Homebrew is installed.'
 
 # Check for Oh My Zsh
-printf 'Installing Oh My Zsh for you.\n'
+printf '\n\nInstalling Oh My Zsh for you.\n'
 ZSH= /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 printf '✅ Oh My Zsh is installed.'
+
+# Install Rosetta 2, for Macs with Apple silicon to use apps built for Intel
+printf '\n\nInstalling Rosetta 2.\n'
+sudo softwareupdate --install-rosetta
+printf '✅ Rosetta 2 is installed.'
+
 
 exit 0
